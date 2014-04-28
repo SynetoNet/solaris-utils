93e3dac7dcb456054881b57c1704a1c592533d62
Dan Vatca <dan.vatca@gmail.com>
2014-04-28 13:25:45 +0300

    iscsiunmap.d will show unmapped LBAs and space in MB.

--- performance/tools/iscsi/iscsiunmap.d
+++ performance/tools/iscsi/iscsiunmap.d
@@ -1,6 +1,4 @@
-#!/usr/sbin/dtrace -s
-
-#pragma D option quiet
+#!/usr/sbin/dtrace -qs
 
 dtrace:::BEGIN
 {
@@ -12,17 +10,18 @@ fbt:stmf_sbd:sbd_unmap:entry
 {
         start_sbd_unmap = timestamp;
         bytes_sbd_handle_unmap_xfer += arg2;
+        printf("    -> UNMAP offset=%x; len=%d\n", arg1, arg2);
 }
 
 fbt:stmf_sbd:sbd_handle_unmap_xfer:entry
 {
-        printf(" > Starting UNMAP command ...\n");
         start_sbd_handle_unmap_xfer = timestamp;
         bytes_sbd_handle_unmap_xfer = 0;
+        printf("UNMAP starting ...\n");
 }
 
 fbt:stmf_sbd:sbd_handle_unmap_xfer:return
 /start_sbd_handle_unmap_xfer > 0/
 {
-        printf(" < UNMAP command took %d ms to free %d bytes\n", (timestamp - start_sbd_handle_unmap_xfer)/1000/1000, bytes_sbd_handle_unmap_xfer);
+        printf("UNMAP took %d ms to free %d MB\n", (timestamp - start_sbd_handle_unmap_xfer)/1000/1000, bytes_sbd_handle_unmap_xfer / 1024 / 1024);
 }
